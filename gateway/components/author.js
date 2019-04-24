3ca5800e3631a43147a0eb400edf49e825bf0a75
Trevor Livingston <tlivings@gmail.com>
2019-04-24 11:35:49 -0500

    cleanup

--- gateway/components/author.js
+++ gateway/components/author.js
@@ -1,40 +1,41 @@
 
 const GraphQLComponent = require('graphql-component');
 
-const types = `
-    # An author.
-    type Author {
+class AuthorComponent extends GraphQLComponent {
+  constructor({ broker }) {
+
+    const types = `
+      # An author.
+      type Author {
         id: ID!
         # The author name.
         name: String,
         # The author email.
         email: String
-    }
-    type Query {
-      # Seach for an author by id.
-      author(id: ID!, version: String) : Author
-    }
-    type Mutation {
+      }
+      type Query {
+        # Seach for an author by id.
+        author(id: ID!, version: String) : Author
+      }
+      type Mutation {
         # Create a new book.
         author(name: String!) : Author
-    }
-`;
+      }
+    `;
 
-const resolvers = {
-  Query: {
-    author(_, { id }, { call }) {
-      return this._broker.call('author.query', { id });
-    }
-  },
-  Mutation: {
-    author(_, { name }, { call }) {
-      return this._broker.call('author.mutate', { name });
-    }
-  }
-};
+    const resolvers = {
+      Query: {
+        author(_, { id }, { call }) {
+          return this._broker.call('author.query', { id });
+        }
+      },
+      Mutation: {
+        author(_, { name }, { call }) {
+          return this._broker.call('author.mutate', { name });
+        }
+      }
+    };
 
-class AuthorComponent extends GraphQLComponent {
-  constructor({ broker }) {
     super({ types, resolvers });
 
     this._broker = broker;
--- gateway/components/book.js
+++ gateway/components/book.js
@@ -2,56 +2,56 @@
 const Author = require('./author');
 const GraphQLComponent = require('graphql-component');
 
-const types = `
-    # This is a book.
-    type Book {
+class BookComponent extends GraphQLComponent {
+  constructor({ broker }) {
+    const authorComponent = new Author({ broker });
+
+    const types = `
+      # This is a book.
+      type Book {
         id: ID!
         # The name of the book.
         name: String,
         # The book's author.
         author: Author
-    }
-    type Query {
-      # Search for a book by id.
-      book(id: ID!) : Book
-    }
-    type Mutation {
+      }
+      type Query {
+        # Search for a book by id.
+        book(id: ID!) : Book
+      }
+      type Mutation {
         # Create a new book.
         book(name: String!, author_id: ID!) : Book
-    }
-`;
+      }
+    `;
 
-const resolvers = {
-  Query: {
-    book(_, { id }, { call }) {
-        return this._broker.call('book.query', { id });
-    }
-  },
-  Mutation: {
-      book(_, { name, author_id }, { call }) {
+    const resolvers = {
+      Query: {
+        book(_, { id }, { call }) {
+          return this._broker.call('book.query', { id });
+        }
+      },
+      Mutation: {
+        book(_, { name, author_id }, { call }) {
           return this._broker.call('book.mutate', { name, author_id });
+        }
+      },
+      Book: {
+        async author(book, args, context, info) {
+
+          const { data, errors } = await this._authorComponent.execute(`query { author(id: "${book.author_id}") { ...AllAuthor }}`, context);
+
+          if (errors) {
+            throw errors[0];
+          }
+
+          return data.author;
+        }
       }
-  },
-  Book: {
-    async author(book, args, context, info) {
-      
-      const { data, errors } = await this._authorComponent.execute(`query { author(id: "${book.author_id}") { ...AllAuthor }}`, context);
+    };
 
-      if (errors) {
-        throw errors[0];
-      }
-    
-      return data.author;
-    }
-  }
-};
+    super({ types, resolvers, imports: [authorComponent] });
 
-class BookComponent extends GraphQLComponent {
-  constructor({ broker }) {
-    const authorComponent = new Author({ broker });
-
-    super({ types, resolvers, imports: [ authorComponent ] });
-    
     this._broker = broker;
     this._authorComponent = authorComponent;
   }
